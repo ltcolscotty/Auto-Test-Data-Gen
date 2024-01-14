74279520f1a0eec079e4072f6aa6333a1752b3f3
Aidan <aidan.yung@gmail.com>
2024-01-13 20:27:24 -0800

    full_comp_list created

---
+++ helper.py
@@ -0,0 +1,16 @@
+from rich.table import Table
+from robot import *
+
+def full_comp_list(robot_list):
+
+    prnt_table = Table("Full competition and attribute list")
+
+    columns = ["name", "ranking", "cycle_multi", "percent", "ground_cap", "soource_cap", "amp_cap", "speaker_cap"]
+
+    for column in columns:
+        prnt_table.add_column(column)
+
+    for bot in robot_list:
+        prnt_table.add_row(bot.get_table_format())
+
+    return prnt_table
